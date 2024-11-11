18caca7cb0a24335a44e99b8891436095973bfab
Hunter Wang <s092284s@gmail.com>
2024-11-12 01:35:59 +0800

    search page

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,3 +1,5 @@
+import { ProductGrid } from "@/components/ProductGrid";
+import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
 import React from "react";
 
 const SearchPage = async ({
@@ -6,7 +8,33 @@ const SearchPage = async ({
   searchParams: { query: string };
 }) => {
   const { query } = await searchParams;
-  return <div>Search Page for {query}</div>;
+  const products = await searchProductsByName(query);
+
+  if (!products?.length) {
+    return (
+      <div className=" flex flex-col items-center justify-start min-h-screen bg-gray-100 p-4">
+        <div className=" bg-white p-8 rounded-xl shadow-md w-full max-w-4xl">
+          <h1 className=" text-3xl font-bold text-center mb-5">
+            No products found for: {query}
+          </h1>
+          <p className=" to-gray-600 text-center">
+            Try Searching with different keywords
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className=" flex flex-col items-center justify-start min-h-screen bg-gray-100 p-4">
+      <div className=" bg-gray-100 p-8 rounded-lg shadow-md w-full ">
+        <h1 className=" text-3xl font-bold mb-6 text-center">
+          Search results for {query}
+        </h1>
+        <ProductGrid products={products} />
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
